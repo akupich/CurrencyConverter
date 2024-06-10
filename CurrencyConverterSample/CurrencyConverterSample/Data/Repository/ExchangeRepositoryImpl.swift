f6cb2312e4bcb53136b7d85ebd60a6019e3653a6
Andriy Kupich <a.kupich@gmail.com>
2024-06-10 13:58:30 +0200

    Presentation layer developed.

--- CurrencyConverterSample/CurrencyConverterSample/Data/Repository/ExchangeRepositoryImpl.swift
+++ CurrencyConverterSample/CurrencyConverterSample/Data/Repository/ExchangeRepositoryImpl.swift
@@ -5,4 +5,19 @@
 //  Created by Andriy Kupich on 02/06/2024.
 //
 
-import Foundation
+import Combine
+
+final class ExchangeRepositoryImpl: ExchangeRepository {
+    private var networkService: NetworkServiceProtocol
+    
+    init(with networkService: NetworkServiceProtocol = NetworkService()) {
+        self.networkService = networkService
+    }
+    
+    func getConversionRate(_ request: ConversionRateRequest) -> AnyPublisher<ExchangeAmount?, NetworkError> {
+        return networkService.request(ExchangeEndpoint.exchange(from: request.sourceCurrency,
+                                                                to: request.targetCurrency,
+                                                                amount: request.amount))
+        .eraseToAnyPublisher()
+    }
+}
